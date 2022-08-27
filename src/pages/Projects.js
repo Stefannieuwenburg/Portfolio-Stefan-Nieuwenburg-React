8da008f4c04eb244e320ff7c17912fd594d9cc58
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-08-28 00:19:14 +0200

    commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -58,27 +58,32 @@ const ProjectBox = styled.div`
 
 
 export default function Projects() {
-    const proj0 = () => {
+    const ViewliveProj0 = () => {
         window.open("https://mulsoft-website-styled-components.netlify.app/");
     };
-    const proj1 = () => {
+    const VieuwCodeProj0 = () => {
+        window.open("https://github.com/Stefannieuwenburg/my-project-mulsoft-website");
+    };
+    const ViewliveProj1 = () => {
         window.open("https://styled-coffee-shop.netlify.app/");
     };
-    const proj2 = () => {
+    const VieuwCodeProj1 = () => {
+        window.open("https://github.com/Stefannieuwenburg/my-project-styled-coffee-shop-website");
+    };
+    const ViewliveProj2 = () => {
         window.open("https://styled-website-team-project.netlify.app/");
     };
-    const proj3 = () => {
+    const VieuwCodeProj2 = () => {
+        window.open("https://github.com/Stefannieuwenburg/my-project-styled-website");
+    };
+    const VieuwCodeProj3 = () => {
         window.open("https://github.com/Stefannieuwenburg/React-blog-site");
     };
-    const proj4 = () => {
-        window.open(
-            "https://github.com/Stefannieuwenburg/React-kids-books-shop"
-        );
+    const VieuwCodeProj4 = () => {
+        window.open("https://github.com/Stefannieuwenburg/React-kids-books-shop");
     };
-    const proj5 = () => {
-        window.open(
-            "https://github.com/Stefannieuwenburg/vite-react-movie-database"
-        );
+    const VieuwCodeProj5= () => {
+        window.open("https://github.com/Stefannieuwenburg/vite-react-movie-database");
     };
 
     
@@ -91,42 +96,45 @@ export default function Projects() {
                         <img src={image0} alt="i" />
                         <Paragraph>React</Paragraph>
                         <Paragraph>project</Paragraph>
-                        <Button onClick={proj0}>View live</Button>
+                        <Button onClick={ViewliveProj0}>View live</Button>
+                        <Button onClick={VieuwCodeProj0}>vieuw code</Button>
                     </ProjectBox>
 
                     <ProjectBox>
                         <img src={image1} alt="i" />
                         <Paragraph>React</Paragraph>
                         <Paragraph>project</Paragraph>
-                        <Button onClick={proj1}>vieuw live</Button>
+                        <Button onClick={ViewliveProj1}>vieuw live</Button>
+                        <Button onClick={VieuwCodeProj1}>vieuw code</Button>
                     </ProjectBox>
 
                     <ProjectBox>
                         <img src={image2} alt="i" />
                         <Paragraph>React</Paragraph>
                         <Paragraph>project</Paragraph>
-                        <Button onClick={proj2}>vieuw live</Button>
+                        <Button onClick={ViewliveProj2}>vieuw live</Button>
+                        <Button onClick={VieuwCodeProj2}>vieuw code</Button>
                     </ProjectBox>
 
                     <ProjectBox>
                         <img src={image3} alt="i" />
                         <Paragraph>React</Paragraph>
                         <Paragraph>project</Paragraph>
-                        <Button onClick={proj3}>vieuw code</Button>
+                        <Button onClick={VieuwCodeProj3}>vieuw code</Button>
                     </ProjectBox>
 
                     <ProjectBox>
                         <img src={image4} alt="i" />
                         <Paragraph>React</Paragraph>
                         <Paragraph>project</Paragraph>
-                        <Button onClick={proj4}>vieuw code</Button>
+                        <Button onClick={VieuwCodeProj4}>vieuw code</Button>
                     </ProjectBox>
 
                     <ProjectBox>
                         <img src={image5} alt="i" />
                         <Paragraph>HTML</Paragraph>
                         <Paragraph>project</Paragraph>
-                        <Button onClick={proj5}>vieuw code</Button>
+                        <Button onClick={VieuwCodeProj5}>vieuw code</Button>
                     </ProjectBox>
                 </ProjectWrapper>
             </ContainerProject>
